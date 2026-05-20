74b9a68a55b5c766c7b3997229fa70d3ae9ca860
Kevin Cua <kevin.cua@umbocv.com>
2026-05-20 16:51:59 +0800

    feat: Update github scripts

--- github/build-debian-amd64.sh
+++ github/build-debian-amd64.sh
@@ -9,7 +9,7 @@ IMAGE="774915305292.dkr.ecr.us-west-2.amazonaws.com/debian-with-opencv:debian-${
 AWS_CLI_VERSION=$(aws --version 2>&1 | cut -d " " -f1 | cut -d "/" -f2 | cut -c 1)
 AWS_REGION=us-west-2
 ECR_REGISTRY=774915305292.dkr.ecr.us-west-2.amazonaws.com
-docker buildx build --pull \
+docker buildx build --pull --load \
   --platform linux/amd64 \
   -t "${IMAGE}" \
   --build-arg OPENCV_VERSION=$OPENCV_VERSION \
